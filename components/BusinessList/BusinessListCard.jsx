95ca637a83e537d49051d12f35f7d4ae4968f4af
ahmdfkhri <fakhri.ahmad030304@gmail.com>
2024-10-01 20:00:24 +0700

    each category list screen

---
+++ components/BusinessList/BusinessListCard.jsx
@@ -0,0 +1,47 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { Colors } from '../../constants/Colors'
+
+export default function BusinessListCard({business}) {
+  return (
+    <View style={{
+      padding: 10, 
+      margin: 10,
+      borderRadius: 15,
+      backgroundColor: '#fff',
+      display: 'flex',
+      flexDirection: 'row',
+      gap: 10,
+    }}>
+      <Image source={{uri: business?.imageUrl}} style={{
+        height: 120,
+        width: 120,
+        borderRadius: 15,
+      }}/>
+      <View style={{flex: 1, gap: 5}}>
+        <Text style={{
+          fontFamily: 'outfit-bold',
+          fontSize: 17,
+        }}>{business.name}</Text>
+        <Text style={{
+          fontFamily: 'outfit',
+          color: Colors.GRAY,
+          fontSize: 15
+        }}>{business.address}</Text>
+        <View style={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: 5,
+          }}>
+            <Image source={require('./../../assets/images/star.png')} style={{
+              height: 15,
+              width: 15,
+            }}/>
+            <Text style={{
+              fontFamily: 'outfit'
+            }}>4.5</Text>
+          </View>
+      </View>
+    </View>
+  )
+}
