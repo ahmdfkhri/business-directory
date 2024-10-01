832253bbe031922a8a7a391308baad41be42f50a
ahmdfkhri <fakhri.ahmad030304@gmail.com>
2024-10-01 09:44:35 +0700

    list of popular business

---
+++ components/Home/PopularBusiness.jsx
@@ -0,0 +1,53 @@
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Colors } from '../../constants/Colors'
+import { collection, getDocs, limit, query } from 'firebase/firestore'
+import { db } from '../../configs/FirebaseConfig'
+import PopularBusinessCard from './PopularBusinessCard'
+
+export default function PopularBusiness() {
+  const [businessList, setBusinessList] = useState([]);
+
+  useEffect(() => {
+    GetPopularBusinessList();
+  }, [])
+
+  const GetPopularBusinessList = async() => {
+    setBusinessList([]);
+    const q = query(collection(db, 'BusinessList'), limit(10));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      setBusinessList(prev => [...prev, doc.data()]);
+    })
+  }
+  return (
+    <View>
+      <View style={{
+        padding: 20,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 10,
+      }}>
+        <Text style={{
+          fontSize: 20,
+          fontFamily: 'outfit-bold',
+        }}>Popular Business</Text>
+        <Text style={{
+          color: Colors.PRIMARY,
+          fontFamily: 'outfit-medium'
+        }}>View All</Text>
+      </View>
+      <FlatList 
+        data={businessList}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item, index}) => (
+          <PopularBusinessCard business={item} key={index}/>
+        )}
+      />
+    </View>
+  )
+}
