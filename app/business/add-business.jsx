f0726aa0f58a477fb7d96da3e29329112e0db9da
felixkur1 <fk494154@gmail.com>
2024-10-06 14:26:24 +0700

    selesai membuat fungsi add business

--- app/business/add-business.jsx
+++ app/business/add-business.jsx
@@ -1,34 +1,39 @@
-import { View, Text, Image, TextInput } from 'react-native'
+import { View, Text, Image, TouchableOpacity, TextInput, ToastAndroid, ActivityIndicator } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useNavigation } from 'expo-router'
 import { Colors } from '../../constants/Colors';
-import { TouchableOpacity } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import RNPickerSelect from 'react-native-picker-select';
-import { collection, getDocs, query } from 'firebase/firestore';
-import { db } from './add-business';
-export {db} from'./../../configs/FirebaseConfig'
+import { db, storage } from '../../configs/FirebaseConfig';
+import { collection, doc, getDocs, query, setDoc } from 'firebase/firestore';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { useUser } from '@clerk/clerk-expo';
 
 export default function AddBusiness() {
-  const navigation=useNavigation();
-  const [image,setImage]=useState(null);
-  const [categoryList,setCategoryList]=useState([]);
+  const navigation = useNavigation();
+  const [image, setImage] = useState(null);
+  const [categoryList, setCategoryList] = useState([]);
+  const {user} = useUser();
 
-  const [name,setName]=useState();
-  const [address,setAddress]=useState();
-  const [contact,setContact]=useState();
-  const [website,setWebsite]=useState();
-  const [about,setAbout]=useState();
-  const [category,setCategory]=useState();
-  useEffect(()=>{
+  const [name, setName] = useState();
+  const [address, setAddress] = useState();
+  const [contact, setContact] = useState();
+  const [website, setWebsite] = useState();
+  const [about, setAbout] = useState();
+  const [category, setCategory] = useState();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
     navigation.setOptions({
-      headerTitle:'Add New Business',
-      headerShown:true,
-    })
-    GetCategoryList();
-  },[])
+      headerTitle: 'Add New Business',
+      headerShown: true,
+    });
 
-  const onImagePick=async()=>{
+    GetCategoryList();
+  }, []);
+
+  const onImagePick = async() => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
@@ -38,145 +43,183 @@ export default function AddBusiness() {
     console.log(result);
   }
 
-  const GetCategoryList=async()=>{
-    setCategoryList([])
-    const q=query(collection(db,'Category'));
-    const snapShot=await getDocs(q);
+  const GetCategoryList = async() => {
+    setCategoryList([]);
+    const q = query(collection(db, 'Category'));
+    const snapShot = await getDocs(q);
 
-    snapShot.forEach((doc)=>{
+    snapShot.forEach((doc) => {
       console.log(doc.data());
-      setCategoryList(prev=>[...prev,{
-        label:(doc.data()).name,
-        value:(doc.data()).name
+      setCategoryList(prev =>  [...prev, {
+        label: (doc.data()).name,
+        value: (doc.data()).name,
       }])
     })
   }
+
+  const onAddNewBusiness = async() => {
+    setLoading(true);
+    const fileName = Date.now().toString() + ".jpg";
+    const resp = await fetch(image);
+    const blob = await resp.blob();
+
+    const imageRef = ref(storage, 'business-app/' + fileName);
+    uploadBytes(imageRef, blob).then((snapshot) => {
+      console.log('File uploaded...');
+    }).then(resp => {
+      getDownloadURL(imageRef).then(async(downloadUrl) => {
+        console.log(downloadUrl);
+        saveBusinessDetail(downloadUrl);
+      })
+    })
+    setLoading(false);
+  }
+
+  const saveBusinessDetail = async(imageUrl) => {
+    await setDoc(doc(db, 'BusinessList', Date.now().toString()), {
+      name: name,
+      address: address,
+      contact: contact,
+      about: about,
+      website: website,
+      category: category,
+      username: user?.fullName,
+      userEmail: user?.primaryEmailAddress?.emailAddress,
+      userImage: user?.imageUrl,
+      imageUrl: imageUrl,
+    })
+
+    setLoading(false)
+    ToastAndroid.show('New business added', ToastAndroid.LONG);
+  }
   return (
-    <View>
+    <View style={{
+      padding: 20
+    }}>
       <Text style={{
-        fontFamily:'outfit-bold',
-        fontSize:25
+        fontFamily: 'outfit-bold',
+        fontSize: 25,
       }}>Add New Business</Text>
       <Text style={{
-        fontFamily:'outfit',
-        color:Colors.GRAY
-      }}>Fill all details in order to add new business</Text>
-      <TouchableOpacity style={{
-        marginTop:20
+        fontFamily: 'outfit',
+        color: Colors.GRAY,
+        fontSize: 15,
+      }}>Fill all details in order to add new business.</Text>
+      <TouchableOpacity  style={{
+        marginTop: 20,
       }}
-      onPress={()=>onImagePick()}
+      onPress={() => onImagePick()}
       >
-      {!image? <Image source={require('./../../assets/images/placeholder.png')}
-        style={{
-            width:100,
-            height:100
-        }}
-      />
-      :
-      <Image source={{uri:image}}
-        style={{
-            width:100,
-            height:100,
-            borderRadius:15
-        }}
-      />}
+        {!image? 
+        <Image source={require('../../assets/images/placeholder.png')} style={{
+          width: 100,
+          height: 100,
+        }}/>:
+        <Image source={{uri: image}} style={{
+          width: 100,
+          height: 100,
+        }}/>
+        }
       </TouchableOpacity>
+
       <View>
-        <TextInput placeholder='Name'
-        onChangeText={(v)=>setName(v)}
+        <TextInput placeholder='Name' 
+        onChangeText={(v) => setName(v)}
         style={{
-          padding:10,
-          borderWidth:1,
-          borderRadius:5,
-          fontSize:17,
-          backgroundColor:'#fff',
-          marginTop:10,
-          borderColor:Colors.PRIMARY,
-          fontFamily:'outfit'
-        }}
-        />
-        <TextInput placeholder='Address'
-        onChangeText={(v)=>setAddress(v)}
+          padding: 15,
+          borderWidth: 1,
+          borderRadius: 5,
+          fontSize: 17,
+          backgroundColor: '#fff',
+          marginTop: 10,
+          borderColor: Colors.PRIMARY,
+          fontFamily: 'outfit'
+        }}/>
+        <TextInput placeholder='Address' 
+        onChangeText={(v) => setAddress(v)}
+        style={{  
+          padding: 15,
+          borderWidth: 1, 
+          borderRadius: 5,
+          fontSize: 17,
+          backgroundColor: '#fff',
+          marginTop: 10,
+          borderColor: Colors.PRIMARY,
+          fontFamily: 'outfit'
+        }}/>
+        <TextInput placeholder='Contact' 
+        onChangeText={(v) => setContact(v)}
         style={{
-          padding:10,
-          borderWidth:1,
-          borderRadius:5,
-          fontSize:17,
-          backgroundColor:'#fff',
-          marginTop:10,
-          borderColor:Colors.PRIMARY,
-          fontFamily:'outfit'
-        }}
-        />
-        <TextInput placeholder='Contact'
-        onChangeText={(v)=>setContact(v)}
-        style={{
-          padding:10,
-          borderWidth:1,
-          borderRadius:5,
-          fontSize:17,
-          backgroundColor:'#fff',
-          marginTop:10,
-          borderColor:Colors.PRIMARY,
-          fontFamily:'outfit'
-        }}
-        />
+          padding: 15,
+          borderWidth: 1, 
+          borderRadius: 5,
+          fontSize: 17,
+          backgroundColor: '#fff',
+          marginTop: 10,
+          borderColor: Colors.PRIMARY,
+          fontFamily: 'outfit'
+        }}/>
         <TextInput placeholder='Website'
-        onChangeText={(v)=>setWebsite(v)}
+        onChangeText={(v) => setWebsite(v)}
         style={{
-          padding:10,
-          borderWidth:1,
-          borderRadius:5,
-          fontSize:17,
-          backgroundColor:'#fff',
-          marginTop:10,
-          borderColor:Colors.PRIMARY,
-          fontFamily:'outfit'
-        }}
-        />
-        <TextInput placeholder='About'
-        onChangeText={(v)=>setAbout(v)}
+          padding: 15,
+          borderWidth: 1, 
+          borderRadius: 5,
+          fontSize: 17,
+          backgroundColor: '#fff',
+          marginTop: 10,
+          borderColor: Colors.PRIMARY,
+          fontFamily: 'outfit'
+        }}/>
+        <TextInput placeholder='About' 
+        onChangeText={(v) => setAbout(v)}
         multiline
-        numberOfLines={5}
+        numberOfLines={3}
         style={{
-          padding:10,
-          borderWidth:1,
-          borderRadius:5,
-          fontSize:17,
-          backgroundColor:'#fff',
-          marginTop:10,
-          borderColor:Colors.PRIMARY,
-          fontFamily:'outfit',
-          height:100
-        }}
-        />
-
+          padding: 15,
+          borderWidth: 1, 
+          borderRadius: 5,
+          fontSize: 17,
+          backgroundColor: '#fff',
+          marginTop: 10,
+          borderColor: Colors.PRIMARY,
+          fontFamily: 'outfit',
+          height: 100,
+        }}/>
         <View style={{
-          borderWidth:1,
-          borderRadius:5,
-          backgroundColor:'#fff',
-          marginTop:10,
-          borderColor:Colors.PRIMARY
+          borderWidth: 1, 
+          borderRadius: 5,
+          backgroundColor: '#fff',
+          marginTop: 10,
+          borderColor: Colors.PRIMARY,
+          fontFamily: 'outfit'
         }}>
-        <RNPickerSelect
-          onValueChange={(value) => setCategory(value)}
-          items={categoryList}
-        />
+          <RNPickerSelect
+            onValueChange={(value) => setCategory(value)}
+            items={categoryList}
+          />
         </View>
-
-        <TouchableOpacity style={{
-          padding:15,
-          backgroundColor:Colors.PRIMARY,
-          borderRadius:5,
-          marginTop:20
-        }}>
-          <Text style={{
-            textAlign:'center',
-            fontFamily:'outfit-medium',
-            color:'#fff'
-          }}>Add new Business</Text>
-        </TouchableOpacity>
       </View>
+      <TouchableOpacity 
+      disabled={loading}
+      onPress={() => onAddNewBusiness()}
+      style={{
+        padding: 15,
+        backgroundColor: Colors.PRIMARY,
+        borderRadius: 5,
+        marginTop: 20,
+      }}>
+        {loading ?
+          <ActivityIndicator size={'large'} color={'#fff'}
+          /> :
+          <Text style={{
+            textAlign: 'center',
+            fontFamily: 'outfit-medium',
+            color: '#fff'
+          }}>Add New Business
+          </Text>
+        }
+      </TouchableOpacity>
     </View>
   )
 }
